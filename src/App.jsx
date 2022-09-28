50ea963c239ee89a740e17471229c985c6ab6e66
Kalu Chinonso <tripletens.kc@gmail.com>
2022-09-28 15:52:50 +0100

    added the pie chart for the dashboard analytics

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,10 @@ import Login from "./pages/login";
 
 const App = () => {
   return (
-    <>
       <Routes>
         <Route path="/" element={<Login />}/>
         <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
-    </>
   );
 };
 
